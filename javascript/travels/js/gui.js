42e0cafd1fd6ef3f8501a6550441269651d118c5
splincode <omaxphp@yandex.ru>
2016-04-01 02:51:37 +0300

    update

---
+++ javascript/travels/js/gui.js
@@ -0,0 +1,5 @@
+
+document.querySelector('.card_add_list').onclick = function(){
+	var data = document.querySelector('.card_add').innerHTML;
+	console.log(document.querySelector('.card_add').innerHTML)
+}
