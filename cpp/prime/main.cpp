cb08fa72eebe922d7679e1d18534be0bb9feb13a
splincode <omaxphp@yandex.ru>
2016-04-15 22:38:10 +0300

    update

--- cpp/prime/main.cpp
+++ cpp/prime/main.cpp
@@ -2,8 +2,12 @@
 
 using namespace std;
 
-int main(int argc, char *argv[])
-{
-    cout << "Hello World!" << endl;
+int main(int argc, char *argv[]){
+
+    std::cout << "Have " << argc << " arguments:" << std::endl;
+    for (int i = 0; i < argc; ++i) {
+            std::cout << argv[i] << std::endl;
+    }
+
     return 0;
 }
