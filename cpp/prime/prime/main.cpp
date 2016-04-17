7f6e24edb1fdae2b6b869b6921a02c25c39dc022
splincode <omaxphp@yandex.ru>
2016-04-18 00:30:42 +0300

    update

---
+++ cpp/prime/prime/main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <biginteger.cpp>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+    if (argc != 3 && argv[1] != "-n") {
+        cout << "неверные входные параметры \n";
+        return 0;
+    }
+
+    BigInteger n(argv[2]), p, q;
+
+    if (n % 2 == 0 || n == 1) {
+        cout << "число не является простым \n";
+        return 0;
+    }
+
+    q = n.sqrt() + 1;
+
+    BigInteger m;
+    bool prm = true;
+
+    // простые делители
+    // начинаются с тройки
+    // p - простые числа Чена
+    // такие простые числа p,
+    // что p+2 либо простое,
+    // либо полупростое
+
+    for(p = 3; p <= q; ) {
+
+        // все четные делители и все делители,
+        // кратные простым числам, могут быть опущены
+        if ( ((p != 2) && (p % 2 == 0)) || ((p != 3) && (p % 3 == 0)) || ((p != 7) && (p % 7 == 0)) ) {
+            p = p + 2; continue;
+        }
+
+        m = n%p;
+        cout << "n = " << n << " p = " << p << ", n % k = " << m <<endl;
+
+        if (n % p == 0) {
+            prm = false;
+            break;
+        }
+
+        p = p + 2;
+    }
+
+    if (prm) cout << "число является простым \n";
+    else cout << "число не является простым \n";
+
+    return 0;
+}
