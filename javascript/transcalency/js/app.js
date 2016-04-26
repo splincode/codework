6b4c5e02976a6c3c40d2d3b9af16592c4dc39ce3
splincode <omaxphp@yandex.ru>
2016-04-26 13:41:38 +0300

    update

--- javascript/transcalency/js/app.js
+++ javascript/transcalency/js/app.js
@@ -8,10 +8,34 @@ $(document).ready(function(){
 	tau = parseFloat( $('.tau').text() );
 	h = parseFloat( $('.h').text() );
 	q = function(x){
-		return 4 * Math.sin(2 * x * Math.PI) * Math.cos(Math.PI * x) * Math.cos(Math.PI * x);
+		var PI = Math.PI;
+		var cos = function(arg){return Math.cos(arg);}
+		var sin = function(arg){return Math.sin(arg);}
+
+		var f = 4*sin(2*PI*x)*cos(PI*x)*cos(PI*x);
+
+		return +f.toFixed(4);
 	}
 
-	//alert(q(100))
+	//alert(q(0.0001));
+
+
+
+	// Canvas
+	var canvas = $('.graph')[0],
+	ctx = canvas.getContext('2d');
+
+	// layer1
+	ctx.fillStyle = "rgb(200,0,0)";
+	ctx.fillRect (0, 0, canvas.width, canvas.height);
+
+
+
+	        
+	        //ctx.fillRect (10, 10, 55, 50);
+
+	        //ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+	        //ctx.fillRect (30, 30, 55, 50);
 
 
 });
