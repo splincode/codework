56c7e5187d64770a97272b4f4c368886edb4bee2
splincode <omaxphp@yandex.ru>
2016-04-28 12:38:39 +0300

    update

---
+++ cpp/compiler/main.cpp
@@ -0,0 +1,17 @@
+#include <cstring>
+#include <iostream>
+#include <string>
+#include <math.h>
+
+using namespace std;
+
+#define var double
+#define function double
+#define read(a) cin >> a;
+#define _ <<
+#define writeln(a, b) cout << a << b << endl;
+#define write(a) cout << a;
+#define program int main(){
+#define end return 0; }
+
+#include "main.psx"
