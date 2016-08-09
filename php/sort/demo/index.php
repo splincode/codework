9cad1a0ebb71cfb7d9484cd4c14f6851f503ba33
splincode <omaxphp@yandex.ru>
2016-08-09 23:41:44 +0300

    update

--- php/sort/demo/index.php
+++ php/sort/demo/index.php
@@ -1,9 +1,12 @@
 <?php
 
-  require 'vendor/WordCounter.php'; 
+  require 'vendor/WordCounter.php';
 
-  $cnt = new Vendor\WordCounter(); // text.txt || text2.txt
-  $arr = $cnt->readFile("text.txt")->arrayWord(); // получаем отсортированный массив
+  $cnt = new Vendor\WordCounter();
+
+  $arr = $cnt
+             ->readFile("text1.txt") // text1.txt || text2.txt
+             ->arrayWord(); // получаем отсортированный массив
 
   foreach ($arr as $key => $value) {
     echo "$value <br>\n";
