6314fc1d89074ee2dc443fc2746f7c89b790cb8f
splincode <omaxphp@yandex.ru>
2016-11-30 02:26:33 +0300

    update

--- javascript/production_system/production_system.js
+++ javascript/production_system/production_system.js
@@ -11,11 +11,16 @@ const math = require('./libs/math.js');
  */
 
 var rl = readline.createInterface(process.stdin, process.stdout);
-var stack = [];
-var stackEval = [];
+var stack = [], stackAlias = [], stackMath = "";
+var roles = db.roles;
 
 var globalCommand = ['ввод фактов> ', 'ввод выражения> '];
 var globalStateMath = false;
+var globalCountMathCommand = 0;
+
+function first(obj) {
+    for (var a in obj) return a;
+}
 
 //var counterCommand = 0;
 
@@ -40,70 +45,156 @@ rl.on('line', function(line) {
 
 	if (globalStateMath) {
 		// в режиме исполнения математических команд
-
-	} else {
-		// классический режим
 		
+		if (line.indexOf("set") != -1) {
+			line = line.replace("set", "");
+			stackMath = stackMath + line + ";";
+			globalCountMathCommand++;
+			console.log(stackMath)
+		} else {
+			console.log(math.eval(stackMath + line))
+		}
 
+		for (let i =0 ; i < roles.length; i++) {
+			if (!roles[i].EXCLUDE && roles[i].MODE == 'EVAL') {
+			
+				let count = 0;
+				let conditions = roles[i].IF;
+				let conditionsState;
+				for (let j = 0; j < conditions.length; j++) {
 
-		
-	}
+					if (conditions[j].indexOf("$") != -1) {
+						conditionsState = conditions[j];
+						
+						for (let option in stackAlias) {
+						 let replaceKey = first(stackAlias[option]);
+						 let value = stackAlias[option][first(stackAlias[option])];
+						 conditionsState = conditionsState.split(replaceKey).join(value);
+						}
+					}
 
+					let checkFormule = false;
+					for (let k = 0; k < stack.length; k++) {
+						if (conditionsState.indexOf("f(") != -1) {
+							try { 
+								if (globalCountMathCommand >= conditionsState.match(new RegExp(",", 'g')).length && !checkFormule) {
+									let result = math.eval(stackMath + conditionsState);
+									if (Boolean(result.entries)) checkFormule = true;
+								}
+								
+							} catch (e) {}
+						} else if (stack[k].line == conditionsState) count++;
 
+					}
 
+					if (checkFormule) count++;
 
-	//counterCommand++;
+					if (count == conditions.length) {
+						roles[i].EXCLUDE = true;
+						
+						stack.push({
+							line: roles[i].THEN,
+							alias: { "$0" : null }
+						});
 
-	/*if (line.replace(/\s/g, "") === '!') {
-		// смена режима
-		globalStateMath = !globalStateMath;
-		rl.setPrompt(globalCommand[Number(globalStateMath)]);
-		rl.prompt();
-		return;
-	}
+						for (let option in stackAlias) {
+						 let replaceKey = first(stackAlias[option]);
+						 let value = stackAlias[option][first(stackAlias[option])];
+						 stack[stack.length-1].line = stack[stack.length-1].line.split(replaceKey).join(value);
+						}
 
+						console.log(stack[stack.length-1].line);
 
+					}
 
-	try {
-		stackEval.push(eval(line));
-	} catch(e){
-		stack.push(line.replace(/\s/g, ""));
-	}
-
-	console.log(db)
-
-	// пробигаемся по правилам
-	let roles = db.roles;
-	for (let i = 0; i < roles.length; i++) {
-		if (!roles[i].EXCLUDE) {
-
-			let count = 0;
-			let conditions = roles[i].IF;
-			//console.log(conditions)
-			for (let j = 0; j < conditions.length; j++) {
-				//console.log(conditions[j])
-				
-				for (let k = 0; k < stack.length; k++) {
-					if (conditions[j] === stack[k]) count++;
 				}
 			}
+		}
 
-			if (count == conditions.length) {
-				roles[i].EXCLUDE = true;
-				stack.push(roles[i].THEN);
-				console.log(roles[i].THEN);
+
+		globalCountMathCommand++;
+		rl.prompt();
+		return;
+
+	} else {
+
+		if (line.replace(/\s/g, "") == "") {
+			rl.prompt();
+			return;
+		}
+
+		if (line.replace(/\s/g, "") == "new") {
+			
+			stack = [];
+			stackAlias = [];
+			for (let i = 0; i < db.roles.length; i++) db.roles[i].EXCLUDE = false;
+
+			rl.prompt();
+			return;
+		}
+
+		// классический режим
+		stack.push({line: line.replace(/\s/g, ""), excludeForFunction: false});
+
+		for (let i = 0; i < roles.length; i++) {
+
+
+			if (!roles[i].EXCLUDE && !roles[i].MODE) {
+				let count = 0;
+				let conditions = roles[i].IF;
+				//console.log(conditions)
+
+				if (typeof conditions == 'function') {
+					for (let k = 0; k < stack.length; k++) {
+		
+						if (conditions(stack[k].line) && !stack[k].excludeForFunction)  { 
+							stackAlias.push(roles[i].THEN(stack[k].line));
+							stack[k].alias = roles[i].THEN(stack[k].line); 
+							stack[k].excludeForFunction = true;
+							roles[i].EXCLUDE = true;
+						}
+					}
+					
+				} else {
+
+
+					for (let j = 0; j < conditions.length; j++) {
+						
+						for (let k = 0; k < stack.length; k++) {
+							let firstKey = first(stack[k].alias);
+							if (stack[k].line == conditions[j].replace(firstKey, stack[k].alias[firstKey])) count++;
+						}
+
+					}
+
+					if (count == conditions.length) {
+
+						roles[i].EXCLUDE = true;
+						stack.push({
+							line: roles[i].THEN,
+							alias: { "$0" : null }
+						});
+
+						for (let option in stackAlias) {
+						 let replaceKey = first(stackAlias[option]);
+						 let value = stackAlias[option][first(stackAlias[option])];
+						 stack[stack.length-1].line = stack[stack.length-1].line.split(replaceKey).join(value);
+						}
+
+						console.log(stack[stack.length-1].line);
+					}
+
+				}
+
+
 			}
-
-			//console.log(count);
 
 
 		}
+
 	}
 
-*/
-
   rl.prompt();
-  //console.log(stack)
 }).on('close',function(){
   process.exit(0);
 });
