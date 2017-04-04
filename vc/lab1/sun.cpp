96b38faf980c2b45c42fb242098ab124632a5fc5
Maxim Ivanov <omaxphp@yandex.ru>
2017-04-04 13:12:07 +0300

    Create sun.cpp

---
+++ vc/lab1/sun.cpp
@@ -0,0 +1,106 @@
+#include <windows.h>
+#include <tchar.h>
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+TCHAR WinName[] = _T("MainFrame");
+
+int APIENTRY WinMain(HINSTANCE This, HINSTANCE Prev, LPSTR cmd, int mode) {
+
+	HWND hWnd; // дескриптор главного окна программы
+	MSG msg; // структура для хранения сообщений
+	WNDCLASS wc; // класс окна
+
+	wc.hInstance = This;
+	wc.lpszClassName = WinName; // имя класса окна
+	wc.lpfnWndProc = WndProc; // функция окна
+	wc.style = CS_HREDRAW / CS_VREDRAW; // cтиль окна
+	wc.hIcon = LoadIcon(NULL, IDC_ARROW); // cтандартный курсор
+	wc.lpszMenuName = NULL; // отключение меню
+	wc.cbClsExtra = 0;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+
+	// заполнение окна белым цветом
+	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1); // установка цвета
+
+	if (!RegisterClass(&wc)) return 0;
+
+	// Создание окна
+	hWnd = CreateWindow(
+		WinName,
+		_T("Каркас Windows-приложения"),
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, // x
+		CW_USEDEFAULT, // y
+		CW_USEDEFAULT, // width
+		CW_USEDEFAULT, // height
+		HWND_DESKTOP, // дескриптор родительского окна
+		NULL, // отключение меню
+		This, // дескриптор приложения
+		NULL // дополнительной информации нет
+	);
+
+	ShowWindow(hWnd, mode);
+
+	while (GetMessage(&msg, NULL, 0, 0)) {
+
+		TranslateMessage(&msg); // трансляция кодов нажатой клавиши
+		DispatchMessage(&msg); // посылает сообщение функции WndProc()
+
+	}
+
+
+	return 0;
+
+}
+
+
+// оконная функция
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+
+
+	PAINTSTRUCT ps;
+	HDC hdc;
+	int x, y;
+	static int sx, sy;
+
+	switch(message) {
+
+
+		case WM_SIZE:
+			sx = LOWORD(lParam);
+			sy = HIWORD(lParam);
+		break;
+
+		case WM_PAINT:
+			hdc = BeginPaint(hWnd, &ps);
+
+			for (x = 0; x < sx; x += sx / 10) {
+				MoveToEx(hdc, x, 0, NULL);
+				LineTo(hdc, x, sy);
+			}
+
+			for (y = 0; y < sy; y += sy / 10) {
+				MoveToEx(hdc, 0, y, NULL);
+				LineTo(hdc, sx, y);
+			}
+
+			EndPaint(hWnd, &ps); 
+		break;
+
+		case WM_DESTROY:
+			PostQuitMessage(0);
+		break;
+
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+	
+	}
+
+
+	return 0;
+
+
+}
+
