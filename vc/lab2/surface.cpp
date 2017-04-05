f7f4fdd3d6ba125b08e5460148c5a7a3936ffe4f
Maxim Ivanov <omaxphp@yandex.ru>
2017-04-05 09:36:22 +0300

    Update surface.cpp

--- vc/lab2/surface.cpp
+++ vc/lab2/surface.cpp
@@ -1,8 +1,7 @@
+#define _USE_MATH_DEFINES
+#include <cmath>
 #include <windows.h>
 #include <tchar.h>
-#include "stdafx.h"
-#include <math.h>
-
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 TCHAR WinName[] = _T("MainFrame");
@@ -17,21 +16,21 @@ int APIENTRY WinMain(HINSTANCE This, HINSTANCE Prev, LPSTR cmd, int mode) {
 	wc.lpszClassName = WinName; // имя класса окна
 	wc.lpfnWndProc = WndProc; // функция окна
 	wc.style = CS_HREDRAW / CS_VREDRAW; // cтиль окна
-	wc.hIcon = LoadIcon(NULL, IDC_ARROW); // cтандартный курсор
+	wc.hIcon = LoadIcon(NULL, IDC_ARROW); // иконка
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW); //  cтандартный курсор
 	wc.lpszMenuName = NULL; // отключение меню
-	wc.cbClsExtra = 0;
 	wc.cbClsExtra = 0;
 	wc.cbWndExtra = 0;
 
 	// заполнение окна белым цветом
-	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1); // установка цвета
+	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1); // установка цвета
 
 	if (!RegisterClass(&wc)) return 0;
 
 	// Создание окна
 	hWnd = CreateWindow(
 		WinName,
-		_T("Каркас Windows-приложения"),
+		_T("Поверхность"),
 		WS_OVERLAPPEDWINDOW,
 		CW_USEDEFAULT, // x
 		CW_USEDEFAULT, // y
@@ -70,7 +69,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
 	double x, y, z;
 	double x_min, x_max, y_min, y_max, Kx, Ky;
 
-	switch (message){
+	switch (message) {
 	case WM_CREATE:
 		hpen = CreatePen(PS_SOLID, 2, RGB(0, 0, 255));
 		hpen1 = CreatePen(PS_SOLID, 2, RGB(0, 0, 255));
@@ -85,8 +84,8 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
 	case WM_PAINT:
 
 		hdc = BeginPaint(hWnd, &ps);
-		a = sx/2;
-		b = sy/2;
+		a = sx / 2;
+		b = sy / 2;
 
 		x_min = -100;
 		x_max = 100;
@@ -94,7 +93,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
 		y_max = 100;
 
 		Kx = (sx - 220) / (x_max - x_min);
-		Ky = (sy- 220) / (y_max - y_min);
+		Ky = (sy - 220) / (y_max - y_min);
 
 		SelectObject(hdc, hpen);
 		MoveToEx(hdc, 0, b, NULL);
@@ -102,21 +101,21 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
 		MoveToEx(hdc, a, 0, NULL);
 
 		LineTo(hdc, a, sy);
-		MoveToEx(hdc, 20, sy-20, NULL);
-		LineTo(hdc, sx-20, 20);
+		MoveToEx(hdc, 20, sy - 20, NULL);
+		LineTo(hdc, sx - 20, 20);
 		n = y_max - y_min;
 
-		for(y = y_min; y < y_max; y += 5) {
-		  
-			hpen1 = CreatePen(PS_SOLID, 2, RGB(255, 255-255/n*(y+y_min), 255-255/n*(y+y_min)));
+		for (y = y_min; y < y_max; y += 5) {
+
+			hpen1 = CreatePen(PS_SOLID, 2, RGB(255, 255 - 255 / n*(y + y_min), 255 - 255 / n*(y + y_min)));
 			SelectObject(hdc, hpen1);
 
-			z = b-100*sin(M_PI/50*sqrt(x_min*x_min + y*y));
-			MoveToEx(hdc, x_min*Kx+a+y, z+y, NULL);
+			z = b - 100 * sin(M_PI / 50 * sqrt(x_min*x_min + y*y));
+			MoveToEx(hdc, x_min*Kx + a + y, z + y, NULL);
 
-			for(x = x_min; x < x_max; x += 5) {
-				z = b-100*sin(M_PI/50*sqrt(x*x +y*y));
-				LineTo(hdc, x*Kx + a+y, z+y);
+			for (x = x_min; x < x_max; x += 5) {
+				z = b - 100 * sin(M_PI / 50 * sqrt(x*x + y*y));
+				LineTo(hdc, x*Kx + a + y, z + y);
 			}
 
 		}
