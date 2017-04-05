a1756ecdceed08a3427494e41537f5a6a9029d9e
Maxim Ivanov <omaxphp@yandex.ru>
2017-04-05 09:32:08 +0300

    Update parabola.cpp

--- vc/lab2/parabola.cpp
+++ vc/lab2/parabola.cpp
@@ -1,8 +1,7 @@
+#define _USE_MATH_DEFINES
+#include <cmath>
 #include <windows.h>
 #include <tchar.h>
-#include "stdafx.h"
-#include <math.h>
-
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 TCHAR WinName[] = _T("MainFrame");
@@ -17,21 +16,21 @@ int APIENTRY WinMain(HINSTANCE This, HINSTANCE Prev, LPSTR cmd, int mode) {
 	wc.lpszClassName = WinName; // имя класса окна
 	wc.lpfnWndProc = WndProc; // функция окна
 	wc.style = CS_HREDRAW / CS_VREDRAW; // cтиль окна
-	wc.hIcon = LoadIcon(NULL, IDC_ARROW); // cтандартный курсор
+	wc.hIcon = LoadIcon(NULL, IDC_ARROW); // иконка
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW); //  cтандартный курсор
 	wc.lpszMenuName = NULL; // отключение меню
-	wc.cbClsExtra = 0;
 	wc.cbClsExtra = 0;
 	wc.cbWndExtra = 0;
 
 	// заполнение окна белым цветом
-	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1); // установка цвета
+	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1); // установка цвета
 
 	if (!RegisterClass(&wc)) return 0;
 
 	// Создание окна
 	hWnd = CreateWindow(
 		WinName,
-		_T("Каркас Windows-приложения"),
+		_T("Парабола"),
 		WS_OVERLAPPEDWINDOW,
 		CW_USEDEFAULT, // x
 		CW_USEDEFAULT, // y
@@ -57,64 +56,66 @@ int APIENTRY WinMain(HINSTANCE This, HINSTANCE Prev, LPSTR cmd, int mode) {
 
 }
 
+
+// оконная функция
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
 	PAINTSTRUCT ps;
 	HDC hdc;
-	
+
 	static int sx, sy;
 	static HPEN hpen1, hpen2;
 	int a, b, x_scr, y_scr;
 	double x, y, h;
 
-	switch(message){
+	switch (message) {
 
-		case WM_CREATE:
-			hpen1 = CreatePen(PS_SOLID, 2, RGB(0, 0, 255));
-			hpen2 = CreatePen(PS_SOLID, 2, RGB(255, 0, 0));
+	case WM_CREATE:
+		hpen1 = CreatePen(PS_SOLID, 2, RGB(0, 0, 255));
+		hpen2 = CreatePen(PS_SOLID, 2, RGB(255, 0, 0));
 		break;
 
-		case WM_SIZE:
-			sx = LOWORD(lParam);
-			sy = HIWORD(lParam);
+	case WM_SIZE:
+		sx = LOWORD(lParam);
+		sy = HIWORD(lParam);
 		break;
 
-		case WM_PAINT:
-			hdc = BeginPaint(hWnd, &ps);
-			a = sx/2;
-			b = sy/2;
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		a = sx / 2;
+		b = sy / 2;
 
-			SelectObject(hdc, hpen1);
-			MoveToEx(hdc, 0, b, NULL);
+		SelectObject(hdc, hpen1);
+		MoveToEx(hdc, 0, b, NULL);
 
-			LineTo(hdc, sx, b);
-			MoveToEx(hdc, a, 0, NULL);
+		LineTo(hdc, sx, b);
+		MoveToEx(hdc, a, 0, NULL);
 
-			LineTo(hdc, a, sy);
-			MoveToEx(hdc, 0, b, NULL);
+		LineTo(hdc, a, sy);
+		MoveToEx(hdc, 0, b, NULL);
 
-			SelectObject(hdc, hpen2);
+		SelectObject(hdc, hpen2);
 
-			MoveToEx(hdc, a, b, NULL);
-			for (x = 0; x <= 100; x++) {
-				LineTo(hdc, a + x, b - 0.1*x*x); 
-			}
+		MoveToEx(hdc, a, b, NULL);
+		for (x = 0; x <= 100; x++) {
+			LineTo(hdc, a + x, b - 0.1*x*x);
+		}
 
-			MoveToEx(hdc, a, b, NULL);
-			for (x = 0; x <= 100; x++) {
-				LineTo(hdc, a - x, b - 0.1*x*x); 
-			}
+		MoveToEx(hdc, a, b, NULL);
+		for (x = 0; x <= 100; x++) {
+			LineTo(hdc, a - x, b - 0.1*x*x);
+		}
 
-			EndPaint(hWnd, &ps);
+		EndPaint(hWnd, &ps);
 		break;
 
-		case WM_DESTROY:
-			DeleteObject(hpen1);
-			DeleteObject(hpen2);
-			PostQuitMessage(0);
+	case WM_DESTROY:
+		DeleteObject(hpen1);
+		DeleteObject(hpen2);
+		PostQuitMessage(0);
 		break;
 
-		default: return DefWindowProc(hWnd, message, wParam, lParam);
-	
+	default: return DefWindowProc(hWnd, message, wParam, lParam);
+
 	}
 	return 0;
 }
