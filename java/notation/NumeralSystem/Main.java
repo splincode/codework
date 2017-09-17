85e7d88b54f7813a1ceab741c0b6bb30c412a9ea
splincode <omaxphp@yandex.ru>
2017-09-17 20:07:18 +0300

    put into a constant

--- java/notation/NumeralSystem/Main.java
+++ java/notation/NumeralSystem/Main.java
@@ -3,16 +3,16 @@ package NumeralSystem;
 import NumeralSystem.NumeralSystemUtil;
 
 public class Main {
-    public static void main(String[] args) {
+ public static void main(String[] args) {
 
-        int decimacal = NumeralSystemUtil.Dec.getNumberFromLine();
-        String hexdecimal = NumeralSystemUtil.Dec.toHex(decimacal);
-        String binary = NumeralSystemUtil.Dec.toBinary(decimacal);
+  int decimacal = NumeralSystemUtil.Dec.getNumberFromLine();
+  String hexdecimal = NumeralSystemUtil.Dec.toHex(decimacal);
+  String binary = NumeralSystemUtil.Dec.toBinary(decimacal);
 
-        System.out.println("==================");
-        System.out.println("Dec: " + decimacal);
-        System.out.println("Hex: " + hexdecimal);
-        System.out.println("Bin: " + binary);
+  System.out.println("==================");
+  System.out.println("Dec: " + decimacal);
+  System.out.println("Hex: " + hexdecimal);
+  System.out.println("Bin: " + binary);
 
-    }
+ }
 }
--- java/notation/NumeralSystem/NumeralSystemUtil.java
+++ java/notation/NumeralSystem/NumeralSystemUtil.java
@@ -5,55 +5,55 @@ import java.util.InputMismatchException;
 
 class NumeralSystemUtil {
 
-	static class Convert {
+ static class Convert {
 
-		public static String digits = "0123456789ABCDEF";
+  public static String digits = "0123456789ABCDEF";
 
-		public static String convertDecToBaseNumber(int decimacal, int scaleBase) {
+  public static String convertDecToBaseNumber(int decimacal, int scaleBase) {
 
-		    if (decimacal <= 0) return "0";
+   if (decimacal <= 0) return "0";
 
-		    int base = scaleBase;
-		    String result = "";
+   int base = scaleBase;
+   String result = "";
 
-		    while (decimacal > 0) {
-		        int digit = decimacal % base;             
-		        result = Convert.digits.charAt(digit) + result; 
-		        decimacal = decimacal / base;
-		    }
+   while (decimacal > 0) {
+    int digit = decimacal % base;
+    result = Convert.digits.charAt(digit) + result;
+    decimacal = decimacal / base;
+   }
 
-		    return result;
-    	}
-	}
+   return result;
+  }
+ }
 
-    static class Dec {
+ static class Dec {
 
-        public static int getNumberFromLine() {
+  public static int getNumberFromLine() {
 
-            Scanner scan = new Scanner(System.in);
-            int decimacal = 0;
+   Scanner scan = new Scanner(System.in);
+   int decimacal = 0;
 
-            System.out.print("Enter decimacal number (integer): ");
+   System.out.print("Enter decimacal number (integer): ");
 
-            try {
-                decimacal = scan.nextInt();
-            } catch (InputMismatchException e) {
-                System.out.println("Input has to be a integer decimacal number...");
-                System.exit(0);
-            }
+   try {
+    decimacal = scan.nextInt();
+   } catch (InputMismatchException e) {
+    System.out.println("Input has to be a integer decimacal number...");
+    System.exit(0);
+   }
 
-            return decimacal;
+   return decimacal;
 
-        }
+  }
 
-        public static String toBinary(int decimacal) {
-        	return NumeralSystemUtil.Convert.convertDecToBaseNumber(decimacal, 2);
-        }
+  public static String toBinary(int decimacal) {
+   return NumeralSystemUtil.Convert.convertDecToBaseNumber(decimacal, 2);
+  }
 
-        public static String toHex(int decimacal) {
-        	return NumeralSystemUtil.Convert.convertDecToBaseNumber(decimacal, 16);
-        }
+  public static String toHex(int decimacal) {
+   return NumeralSystemUtil.Convert.convertDecToBaseNumber(decimacal, 16);
+  }
 
-    }
+ }
 
 }
