57b30532d0293a5ad885347062845fedf3062fbc
splincode <omaxphp@yandex.ru>
2017-09-21 19:43:16 +0300

    update

--- java/garden/ru/splincode/garden/Main.java
+++ java/garden/ru/splincode/garden/Main.java
@@ -1,57 +1,57 @@
 package ru.splincode.garden;
 
 public class Main {
- public static void main(String[] args) {
+    public static void main(String[] args) {
 
- 	int maxTreeInGarden = 20;
- 	GardenTree gardenTrees[] = getGardenTrees(maxTreeInGarden);
- 	showInformationByGardenTrees(gardenTrees);
- 	
- }
+        int maxTreeInGarden = 20;
+        GardenTree gardenTrees[] = getGardenTrees(maxTreeInGarden);
+        showInformationByGardenTrees(gardenTrees);
 
- public static int random(int max) {
- 	return (int) (Math.random() * ++max);
- }
+    }
 
- public static GardenTree[] getGardenTrees(int maxTreeInGarden) {
+    public static int random(int max) {
+        return (int)(Math.random() * ++max);
+    }
 
- 	GardenTreeFactory varietyOfTrees[] = { 
- 		new AppleTreeFactory(), 
- 		new CherryTreeFactory(),
- 		new PearTreeFactory()
- 	};
+    public static GardenTree[] getGardenTrees(int maxTreeInGarden) {
+
+        GardenTreeFactory varietyOfTrees[] = {
+            new AppleTreeFactory(),
+            new CherryTreeFactory(),
+            new PearTreeFactory()
+        };
 
 
- 	int maxIndexStorageGarden = varietyOfTrees.length - 1;
- 	GardenTree gardenTrees[] = new GardenTree[maxTreeInGarden];
+        int maxIndexStorageGarden = varietyOfTrees.length - 1;
+        GardenTree gardenTrees[] = new GardenTree[maxTreeInGarden];
 
- 	for (int i = 0; i < maxTreeInGarden; i++) {
- 		GardenTreeFactory variety = varietyOfTrees[random(maxIndexStorageGarden)];
- 		int currentAge = random(100);
+        for (int i = 0; i < maxTreeInGarden; i++) {
+            GardenTreeFactory variety = varietyOfTrees[random(maxIndexStorageGarden)];
+            int currentAge = random(100);
 
- 		GardenTree tree = variety.createTree(currentAge);
- 		gardenTrees[i] = tree;
- 	}
+            GardenTree tree = variety.createTree(currentAge);
+            gardenTrees[i] = tree;
+        }
 
- 	return gardenTrees;
+        return gardenTrees;
 
- }
+    }
 
- public static void showInformationByGardenTrees(GardenTree gardenTrees[]) {
+    public static void showInformationByGardenTrees(GardenTree gardenTrees[]) {
 
- 	int index = 1;
+        int index = 1;
 
- 	for (GardenTree tree : gardenTrees) {
- 		System.out.print("№" + (index++) + ", ");
- 		System.out.print("сорт: " + tree.getName() +  ", ");
- 		System.out.print("возраст: " + tree.getAge() + " лет, ");
+        for (GardenTree tree: gardenTrees) {
+            System.out.print("№" + (index++) + ", ");
+            System.out.print("сорт: " + tree.getName() + ", ");
+            System.out.print("возраст: " + tree.getAge() + " лет, ");
 
- 		if (tree.freePlant()) {
- 			System.out.println("пригодно для пересадки");
- 		} else {
- 			System.out.println("не пригодно для пересадки");
- 		}
- 	}
- }
+            if (tree.freePlant()) {
+                System.out.println("пригодно для пересадки");
+            } else {
+                System.out.println("не пригодно для пересадки");
+            }
+        }
+    }
 
 }
